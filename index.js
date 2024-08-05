329d33611d8e2b5fa50f6d286ed96f7af5d6efdf
Mufid-031 <mufidmaxsus17@gmail.com>
2024-08-05 19:55:39 +0700

    tes

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const createProduct = require('./routes/createproduct.routes')
-const getProducts = require('./routes/getproducts.routes')
-const deleteProduct = require('./routes/deleteproduct.routes')
-const updateProduct = require('./routes/updateproduct.routes')
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const createProduct = require("./src/routes/createproduct.routes");
+const getProducts = require("./src/routes/getproducts.routes");
+const deleteProduct = require("./src/routes/deleteproduct.routes");
+const updateProduct = require("./src/routes/updateproduct.routes");
+require("dotenv").config();
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 // const prisma = new PrismaClient()
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('tes')
-})
+app.get("/", (req, res) => {
+  res.render("tes");
+});
 
-app.use(createProduct)
-app.use(getProducts)
-app.use(deleteProduct)
-app.use(updateProduct)
+app.use(createProduct);
+app.use(getProducts);
+app.use(deleteProduct);
+app.use(updateProduct);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
