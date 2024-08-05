0f06f9a76ee7caa761d6fc6b875f0eddc9f77b6c
Mufid-031 <mufidmaxsus17@gmail.com>
2024-08-06 02:19:27 +0700

    tes

--- src/routes/getnameproduct.routes.js
+++ src/routes/getnameproduct.routes.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const searchModel = require("../models/search.model");
 
 router.get("/products", async (req, res) => {
+  try {
     const { nameProduct } = req.query;
+    const products = await searchModel.getProductByName(nameProduct);
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-    try {
-        const products = await searchModel.getProductByName(nameProduct);
-        res.status(200).json(products);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message });
-    }
-})
-
-module.exports = router
+module.exports = router;
